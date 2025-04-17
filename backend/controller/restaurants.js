7fd27148eb4d4a3ae782046373a205020a8a0bbc
pok1220 <pokkung1220@gmail.com>
2025-04-17 16:10:53 +0700

    fix controller

--- backend/controller/restaurants.js
+++ backend/controller/restaurants.js
@@ -142,7 +142,8 @@ exports .putRestaurant= async (req,res,next)=>{
             const buffer = image.buffer
             const mimetype = image.mimetype
             let imageKey=""
-            if(!findRestraurant || findRestraurant.picture==""){
+            console.log(findRestraurant)
+            if(findRestraurant && findRestraurant.picture!=""){
                 imageKey=findRestraurant.picture
             }else{
                 imageKey = cloudService.getKeyName()
@@ -150,7 +151,7 @@ exports .putRestaurant= async (req,res,next)=>{
             const {url} = await cloudService.getUrlWithImageNameAndUploadToCloud(buffer, mimetype, imageKey)
             req.body.picture=imageKey;
         }
-        console.log(req.body)
+        // console.log(req.body)
         const restaurant = await Restaurant.findByIdAndUpdate(req.params.id,req.body,{
             new:true,
             runValidators:true,
